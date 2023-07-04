9372c1d314814622a423f50a9cf1a1feedee07a8
SO 高林秀 <s.takabayashi@super-one.co.jp>
2023-07-04 18:03:21 +0900

    初期サンプル作成。データ型とか整備

--- OCR-Sample/sample.py
+++ OCR-Sample/sample.py
@@ -1,20 +1,36 @@
 from __future__ import annotations
 from typing import Final as const
 
+import sys
+from dataclasses import dataclass
 import easyocr
 from easyocr import Reader
 
 
-def recognition() -> list:
+@dataclass
+class RecognitionTextData:
+    """
+    easyocr.readtext()の戻り値のデータクラス
+    """
+    point: list[list[int]]
+    text: str
+    confidence: float
+
+
+def recognition(filename: str) -> list[tuple[RecognitionTextData]]:
     reader: Reader = easyocr.Reader(
         lang_list=['ja'], 
         gpu=False
     )
-    result = reader.readtext('sheet1.jpg')
+    result = reader.readtext(filename)
     return result
 
 def main() -> None:
-    result = recognition()
+    filename: str = sys.argv[1]
+    if filename is None:
+        raise ValueError("ファイル名を指定してください")
+    
+    result = recognition(filename=filename)
     for i in result:
         print(i)
     

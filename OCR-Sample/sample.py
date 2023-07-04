7867e51240d0345bd814bfb97150ec16044a862e
syu <syukunt@gmail.com>
2023-07-04 16:12:41 +0900

    Update sample.py

--- OCR-Sample/sample.py
+++ OCR-Sample/sample.py
@@ -1,6 +1,19 @@
 from __future__ import annotations
 from typing import Final as const
 
+import easyocr
+from easyocr import Reader
 
-def main():
-    print("TEST")
+
+def recognition() -> list:
+    reader: Reader = easyocr.Reader(
+        lang_list=['ja'], 
+        gpu=False
+    )
+    result = reader.readtext('sheet1.jpg')
+    return result
+
+def main() -> None:
+    print(f'認識結果: {recognition()}')
+    
+    

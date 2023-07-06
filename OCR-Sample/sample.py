d8d68f825de5bcf2f91e5273231938877ad4186c
SO 高林秀 <s.takabayashi@super-one.co.jp>
2023-07-06 18:02:48 +0900

    デバッグ用ファイル追加。
    ・認識結果をmatplotlibで表示させる関数を追加。

--- OCR-Sample/sample.py
+++ OCR-Sample/sample.py
@@ -5,6 +5,11 @@ import sys
 from dataclasses import dataclass
 import easyocr
 from easyocr import Reader
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import matplotlib.patches as patches
+import japanize_matplotlib
 
 
 @dataclass
@@ -15,6 +20,25 @@ class RecognitionTextData:
     point: list[list[int]]
     text: str
     confidence: float
+
+
+def show_recognition_image(result: list[tuple(RecognitionTextData)], filename: str) -> None:
+    """
+    認識結果を画像で表示する
+    """
+    image: Image = Image.open(filename)
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.imshow(image)
+    for i in result:
+        try:
+            bbox = np.array(i[0])
+            print(i[1])
+            ax.text(bbox[0,0], bbox[0,1], i[1], size=30, fontname="MS Gothic",color="black")
+            r = patches.Rectangle(xy=(bbox[0,0], bbox[0,1]), width=(bbox[2,0] - bbox[0,0]), height=(bbox[2,1] - bbox[0,1]), ec='g', fill=False,linewidth='10.0')
+            ax.add_patch(r)
+        except:
+            continue
+    plt.show()
 
 
 def recognition(filename: str) -> list[tuple[RecognitionTextData]]:
@@ -33,5 +57,7 @@ def main() -> None:
     result = recognition(filename=filename)
     for i in result:
         print(i)
+    
+    show_recognition_image(result=result, filename=filename)
     
     

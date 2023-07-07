bac39f060306c6e730b22a1e1171e78316d3de47
SO 高林秀 <s.takabayashi@super-one.co.jp>
2023-07-07 15:19:42 +0900

    opencvによるマーク検出を検証

--- OCR-Sample/sample.py
+++ OCR-Sample/sample.py
@@ -10,7 +10,7 @@ from PIL import Image
 import numpy as np
 import matplotlib.patches as patches
 import japanize_matplotlib
-
+import cv2
 
 @dataclass
 class RecognitionTextData:
@@ -20,6 +20,35 @@ class RecognitionTextData:
     point: list[list[int]]
     text: str
     confidence: float
+
+
+def detect_checkbox(filename: str) -> None:
+    """
+    チェックボックスを検出する
+    """
+
+    # Load image
+    image = cv2.imread(filename, 0)
+    # Perform edge detection
+    edges = cv2.Canny(image, 30, 100)
+    # Perform a dilation and erosion to close gaps in between object edges
+    dilated_edges = cv2.dilate(edges, None, iterations=2)
+    edges = cv2.erode(dilated_edges, None, iterations=1)
+    # Find contours in the edge image
+    contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        # Approximate the contour
+        epsilon = 0.02 * cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, epsilon, True)
+        # If the contour has 2 vertices, it could be a checkmark
+        if len(approx) == 2:
+            # Draw the contour on the image
+            cv2.drawContours(image, [contour], -1, (255, 0, 0), 3)
+    # Display the image
+    cv2.imshow('Image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 
 def show_recognition_image(result: list[tuple(RecognitionTextData)], filename: str) -> None:
@@ -32,21 +61,28 @@ def show_recognition_image(result: list[tuple(RecognitionTextData)], filename: s
     for i in result:
         try:
             bbox = np.array(i[0])
-            print(i[1])
-            ax.text(bbox[0,0], bbox[0,1], i[1], size=30, fontname="MS Gothic",color="black")
-            r = patches.Rectangle(xy=(bbox[0,0], bbox[0,1]), width=(bbox[2,0] - bbox[0,0]), height=(bbox[2,1] - bbox[0,1]), ec='g', fill=False,linewidth='10.0')
+            print("ax.text({},{},{})".format(type(bbox[0,0]), type(bbox[0,1]), type(i[1])))
+            ax.text(bbox[0,0], bbox[0,1], i[1], size=10, color="blue")
+            r = patches.Rectangle(
+                xy=(bbox[0,0], bbox[0,1]), 
+                width=(bbox[2,0] - bbox[0,0]), 
+                height=(bbox[2,1] - bbox[0,1]), 
+                ec='g', 
+                fill=False,
+                linewidth=2.0)
             ax.add_patch(r)
         except:
             continue
     plt.show()
+    print("showed")
 
 
-def recognition(filename: str) -> list[tuple[RecognitionTextData]]:
+def recognition(filename: str, allow_list: list[str]=[]) -> list[tuple[RecognitionTextData]]:
     reader: Reader = easyocr.Reader(
-        lang_list=['ja'], 
+        lang_list=['ja', 'en'], 
         gpu=False
     )
-    result = reader.readtext(filename)
+    result = reader.readtext(filename, allowlist=allow_list, decoder='wordbeamsearch', detail=True)
     return result
 
 def main() -> None:
@@ -58,6 +94,7 @@ def main() -> None:
     for i in result:
         print(i)
     
-    show_recognition_image(result=result, filename=filename)
+    #show_recognition_image(result=result, filename=filename)
+    detect_checkbox(filename=filename)
     
     

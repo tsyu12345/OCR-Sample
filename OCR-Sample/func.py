f8f7583ba4a0fab648b26381bb7d9aa89f4a8875
SO 高林秀 <s.takabayashi@super-one.co.jp>
2023-07-24 17:08:44 +0900

    F-RCNNによる、チェックマーク検出を目指す。

---
+++ OCR-Sample/func.py
@@ -0,0 +1,7 @@
+import torch
+
+def collate_fn(batch):
+    images = [item[0] for item in batch]
+    targets = [item[1] for item in batch]
+    images = torch.stack(images, dim=0)
+    return images, targets
